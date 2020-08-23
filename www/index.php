c3f71c5f3bb5f9ec6d695b00f560d5d18aef98ad
Vladimir Remeniuk <vlremenyuk@yandex.ru>
2020-08-23 22:03:26 +0300

    finished front controller

--- www/index.php
+++ www/index.php
@@ -9,5 +9,26 @@ spl_autoload_register('my_autoload');
 
 $route = $_GET['route'] ?? '';
 $routes = require __DIR__ . '/../src/routes.php';
-var_dump($routes);
 
+
+$isRouteFound = false;
+foreach ($routes as $pattern => $controllerAndAction) {
+    preg_match($pattern, $route, $matches);
+    if(!empty($matches)) {
+        $isRouteFound = true;
+        break;
+    }
+}
+
+unset($matches[0]);
+
+if(!$isRouteFound) {
+    echo 'Страница не найдена!';
+    return;
+}
+
+$controllerName = $controllerAndAction[0];
+$actionName = $controllerAndAction[1];
+
+$controller = new $controllerName();
+$controller->$actionName(...$matches);
